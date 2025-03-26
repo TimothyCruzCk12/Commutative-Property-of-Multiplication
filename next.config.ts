483298d7b9bd7d10b40c5ffc79c9d119d5a238e8
AdrianSalmonInteractives <adrian.salmon@ck12.org>
2025-03-26 13:51:32 -0700

    Update Next.js config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Commutative-Property-of-Multiplication',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
